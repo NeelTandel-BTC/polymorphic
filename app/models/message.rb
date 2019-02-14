01793acebf09d256a4474eb88a514339736eeaa9
NeelTandel-BTC <neel.tandel@botreetechnologies.com>
2019-02-14 10:27:07 +0530

    add syntax for add column to polymorphic table

--- app/models/message.rb
+++ app/models/message.rb
@@ -1,3 +1,8 @@
+# Model for Message
 class Message < ApplicationRecord
   belongs_to :sender, polymorphic: true
+
+  # sample for create Message
+  Customer.find(2).messages.create(content:"customer3")
+  Customer.find(2).messages << Message.create(content:"customer2")
 end
